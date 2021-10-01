34168a786798fb215d5c45646922af5a40b41ded
Harold Torres Marino <haroldtm55@gmail.com>
2021-10-01 17:45:49 +1000

    Code along completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: "My Post", description: "My post desc")
+Post.create(title: "My Post 2", description: "My post desc 2")
